51cc7c40a47fe57e4855b28d6104b248fa08fe4a
izludec <izludec@hotmail.com>
2016-11-20 15:08:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,11 @@ var app = app.get('/',function (req, res) {
 })
 
 app = app.get('/*',function (req, res) {
-   //console.log(req.url)
-   //console.log('---------------------')
    var mass = []
    var count = 0
    mongo.connect("mongodb://izludec:123456789data@ds147267.mlab.com:47267/izludec", 
     function(err, db){
      if(err){console.log(err)}
-     //db.collection("links").remove({})
      if((!String(req.url)[req.url.length-1].match("/"))
      &&req.url.split("/")[1].match("new")){
       res.redirect("https://shortener-izludec.herokuapp.com"+req.url+"/")
@@ -35,14 +32,11 @@ app = app.get('/*',function (req, res) {
        res.end(String(app.count))
       })
      }else{
-     // console.log(String(req.url).replace("/",""))
-     //console.log(parseInt(String(req.url).replace("/","")))
      db.collection("links").find({
       _id: (parseInt(String(req.url).replace("/",""))-1)
      }).toArray(
        function(err,doc){
         for(var i in doc){mass.push(doc[i])}
-        //console.log(mass)
         res.redirect(mass[0].url)
        })
      }
